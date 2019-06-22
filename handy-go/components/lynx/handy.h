e3db2038ba0064b3ed4126b4b413cbe6ce31f548
Per Sterner <Per.Sterner@cjt.de>
2019-06-22 23:26:18 +0200

    Lynx emulator

---
+++ handy-go/components/lynx/handy.h
@@ -0,0 +1,23 @@
+#pragma once
+/*
+#include <stdlib.h>
+#include <stdio.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+*/
+#include "handy-go.h"
+
+typedef enum retro_key retro_key;
+typedef struct retro_log_callback retro_log_callback;
+typedef enum retro_pixel_format retro_pixel_format;
+typedef struct retro_game_info retro_game_info;
+typedef enum retro_log_level retro_log_level;
+typedef struct retro_system_info retro_system_info;
+
+/*
+#ifdef __cplusplus
+}
+#endif
+*/
